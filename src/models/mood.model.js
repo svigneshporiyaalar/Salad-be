18ec2edc1b0a8dd323642f34770cc424ff82b80e
vignesh <vignesh@poriyaalar.com>
2023-01-31 10:57:13 +0530

    changed mood datatype

--- src/models/mood.model.js
+++ src/models/mood.model.js
@@ -2,12 +2,11 @@ module.exports = (sequelize, Sequelize) => {
     return sequelize.define("mood", {
       id: {
         type: Sequelize.INTEGER,
-        primaryKey: true,
         autoIncrement: true,
       },
       moodId: {
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4,
+        type: Sequelize.INTEGER,
+        primaryKey: true,
       },
       mood: {
         type: Sequelize.STRING,
