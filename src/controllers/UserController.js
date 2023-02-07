b28cdf88e284c7349b4c10d669654fe40d571d23
vignesh <vignesh@poriyaalar.com>
2023-02-07 16:02:45 +0530

    added partner list API

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -14,13 +14,14 @@ const addPartner = async (ctx) => {
   let { data, userData, reData, partnerId, message } = {};
   let error = null;
   const {user, body}=ctx.request;
-  const { partner_number , relation } = body;
+  const { name, partner_number , relation } = body;
   const userId = _.get(user, "userId" );
   try {
     data = await Partner.findOne({
       raw: true,
       where: {
         contactNumber: partner_number,
+        onboardingComplete: "true"
       },
     });
     console.log(data);
@@ -82,7 +83,7 @@ const removePartner = async (ctx) => {
 };
 
 const partnerList = async (ctx) => {
-  let {data, partnerIds, partnerData} ={}
+  let {data, partnerIds, partnerData , relation , userData} ={}
   let error = null
   const { user } = ctx.request;
   const userId = _.get(user, "userId");
@@ -102,20 +103,28 @@ const partnerList = async (ctx) => {
     partnerIds= data.map((element) =>{
       return element.partnerId
     })
-    console.log("partnerIds List:" ,partnerIds)
+    console.log("partnerIds List:" ,partnerIds )
     partnerData = await Partner.findAll({
       raw:true,
       where:{
         partnerId: partnerIds,
-        onboardingComplete: "true"
-      },
-      order:[["createdAt", "DESC"]]
+      }
     })
+     relation = await Userpartner.findAll({
+      raw:true,
+      where:{
+        userId:userId,
+        partnerId: partnerIds
+      }
+    })
+    console.log(relation)
+    userData = partnerData.map((item) => 
+    ({...item, ...relation.find(itm => itm.partnerId === item.partnerId)}));  
   } catch (err) {
     error = err;
     ctx.response.status = HttpStatusCodes.BAD_REQUEST;
   }
-  ctx.body = responseHelper.buildResponse(error, partnerData);
+  ctx.body = responseHelper.buildResponse(error, userData);
   ctx.response.status = HttpStatusCodes.SUCCESS;
 }
 
